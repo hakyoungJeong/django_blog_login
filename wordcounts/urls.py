6dc8e3df6379aec36c827617462fce519ee08e97
hakyoungJeong <hakyoung1215@gmail.com>
2020-05-20 16:22:35 +0900

    git ignore 적용

---
+++ wordcounts/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.home, name = "home"),
+    path('about',views.about, name = "about"),
+    path('result/',views.result, name = "result"),
+   
+]
