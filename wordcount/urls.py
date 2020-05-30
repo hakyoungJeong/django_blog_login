146cb50424e412555fb5e0928a1cbf67b7a599d8
hakyoungJeong <hakyoung1215@gmail.com>
2020-05-30 23:01:31 +0900

    5주차 과제

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 import wordcounts.urls
 import blog.urls
+import account.urls
 from django.conf import settings
 from django.conf.urls.static import static #이거랑 위에거 미디어파일쓸때 꼭넣어줘야함
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('',blog.views.list, name = "list"),
     path('blog/',include(blog.urls)),
     path('wordcounts/',include(wordcounts.urls)),
+    path('account/',include(account.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
